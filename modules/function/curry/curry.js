a63765dd89df6d2d1b56210dcf2493436f98119b
Po Chun, Lu <oceanus11034@gmail.com>
2019-02-17 14:56:31 +0800

    fix curry for eslint format

--- modules/function/curry/curry.js
+++ modules/function/curry/curry.js
@@ -4,7 +4,7 @@ function curry(fn, ...args) {
   if (args.length >= fn.length) {
     return fn(...args);
   }
-  return (...args2) => curry(fn, ...args, ...args2)
+  return (...args2) => curry(fn, ...args, ...args2);
 }
 
 
