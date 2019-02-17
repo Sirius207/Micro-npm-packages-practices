68a2adc4b75c44dafdac1835dbef5962d136e156
Po Chun, Lu <oceanus11034@gmail.com>
2019-02-17 14:56:00 +0800

    add multiple args & zero args test

--- modules/function/curry/test/curry.test.js
+++ modules/function/curry/test/curry.test.js
@@ -18,6 +18,27 @@ describe('curry', () => {
     ans.equal(sum3(1, 2, 3), sum3C(1)(2)(3));
   });
 
+  it('should allow multiple argument', () => {
+    const sum3 = (a, b, c) => a + b + c;
+    const sum3C = curry(sum3);
+
+    ans.equal(sum3(1, 2, 3), sum3C(1, 2)(3));
+    ans.equal(sum3(1, 2, 3), sum3C(1)(2, 3));
+  });
+
+  it('should allow zero argument', () => {
+    const none = () => {};
+
+    ans.equal(none(), undefined);
+  });
+
+  it('should drop extra arguments', () => {
+    const add = (a, b) => a + b;
+    const addC = curry(add);
+
+    ans.deepEqual(addC(1, 2, 3, 4, 5), 3);
+  });
+
   it('should be pure', () => {
     const add = curry((a, b) => a + b);
     const add1 = add(1);
@@ -33,13 +54,4 @@ describe('curry', () => {
     ans.equal(add2(3), 5);
     ans.equal(add2(4), 6);
   });
-
-  it('should drop extra arguments', () => {
-    const add = (a, b) => a + b;
-    const addC = curry(add);
-
-    ans.deepEqual(addC(1, 2, 3, 4, 5), 3);
-  });
-
-  it('should allow zero argument', () => {});
 });
