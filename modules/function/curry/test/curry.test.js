c97bb1622ef60f955396f7a3eb0409970b94a169
Po Chun, Lu <oceanus11034@gmail.com>
2019-02-17 14:50:52 +0800

    add curry pure & extra args test

--- modules/function/curry/test/curry.test.js
+++ modules/function/curry/test/curry.test.js
@@ -18,9 +18,28 @@ describe('curry', () => {
     ans.equal(sum3(1, 2, 3), sum3C(1)(2)(3));
   });
 
-  it('should be pure', () => {});
+  it('should be pure', () => {
+    const add = curry((a, b) => a + b);
+    const add1 = add(1);
+    const add2 = add(2);
 
-  it('should drop extra arguments', () => {});
+    ans.equal(add1(1), 2);
+    ans.equal(add1(2), 3);
+    ans.equal(add1(3), 4);
+    ans.equal(add1(4), 5);
+
+    ans.equal(add2(1), 3);
+    ans.equal(add2(2), 4);
+    ans.equal(add2(3), 5);
+    ans.equal(add2(4), 6);
+  });
+
+  it('should drop extra arguments', () => {
+    const add = (a, b) => a + b;
+    const addC = curry(add);
+
+    ans.deepEqual(addC(1, 2, 3, 4, 5), 3);
+  });
 
   it('should allow zero argument', () => {});
 });
