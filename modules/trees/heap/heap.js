1107accd22f351cd0a5a100ee5fe6b4158b137c4
Po Chun, Lu <oceanus11034@gmail.com>
2019-03-01 17:07:46 +0800

    update heap basic function case

--- modules/trees/heap/heap.js
+++ modules/trees/heap/heap.js
@@ -5,23 +5,35 @@ class Heap {
     this.right = null;
   }
 
-  insert(value) {}
   // needed for insert
-  sift_up() {}
+  siftUp() {}
+
+  insert(value, heapType = 'max') {}
+
   // returns the max item, without removing it
-  get_max() {}
+  getMax() {}
   // return number of elements stored
-  get_size() {}
+  getSize() {}
   // returns true if heap contains no elements
-  is_empty() {}
-  // returns the max item, removing it
-  extract_max() {}
+  isEmpty() {}
+
   // needed for extract_max
-  sift_down() {}
+  siftDown() {}
+  // returns the max item, removing it
+  extractMax() {}
+
   // removes item at index x
   remove(i) {}
+
   // create a heap from an array of elements, needed for heap_sort
-  heapify() {}
+  heapify() {
+
+  }
+
   // take an unsorted array and turn it into a sorted array in-place using a max heap
-  heap_sort() {}
+  heapSort() {}
+
+  toArray() {}
 }
+
+module.exports = Heap;
